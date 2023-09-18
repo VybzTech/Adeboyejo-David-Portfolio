0994b05ebe98b7b4420768686dd12751d2d63c72
Dharvo <adedave77@gmail.com>
2023-09-18 18:08:50 +0100

    modified:   src/PAGES/CONTACT/Contact.js 	modified:   src/SCSS/App.scss

--- src/PAGES/CONTACT/Contact.js
+++ src/PAGES/CONTACT/Contact.js
@@ -8,25 +8,6 @@ import React, { useState, useEffect } from "react";
 import Pleasantries from "./Pleasantries";
 
 const Contact = () => {
-  // const [anime, setAnime] = useState(false);
-
-  // useEffect(() => {
-  //   var observer = new IntersectionObserver(onIntersection, {
-  //     root: null,
-  //     threshold: 0.5,
-  //   });
-
-  //   function onIntersection(entries) {
-  //     entries.forEach(() => {
-  //       window.addEventListener("scroll", () => {
-  //         if (window.scrollY >= 30) {
-  //           setAnime(true);
-  //         }
-  //       });
-  //     });
-  //   }
-  //   observer.observe(document?.querySelector("main"));
-  // }, []);
 
   return (
     <div id="contact">
@@ -34,7 +15,6 @@ const Contact = () => {
       {/*  AttentionSeeker  Bounce  Fade  Flip Hinge JackInTheBox Roll Rotate Slide Zoom */}
       <main>
         {/* <ContactImage />   */}
-        {/* </Fade> */}
         <ContactMe />
       </main>
       <Footer />
