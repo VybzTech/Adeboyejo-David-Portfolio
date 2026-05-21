8cb51f55874cbbcb48f191305cbcee97f7442306
David Adeboyejo <adedave77@gmail.com>
2026-05-21 12:24:54 +0100

    Removed v0

--- v0/components/sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Hero } from './Hero';
-export { default as WhatIDo } from './WhatIDo';
-export { default as AboutPreview } from './AboutPreview';
-export { default as FeaturedCaseStudies } from './FeaturedCaseStudies';
-export { default as Contact } from './Contact';
